f300f133de67fc102e12480b0031f4f0fb8d0d37
JonathanDC64 <nintendo_jon64@hotmail.com>
2018-01-10 12:03:01 -0500

    small progress

--- Labs/Lab1/Code/Lab1/Lab1/get.py
+++ Labs/Lab1/Code/Lab1/Lab1/get.py
@@ -1,5 +1,7 @@
 import request
 
-def get(host: str, path: str) -> str:
-    message = f'GET {path} HTTP/1.0\r\nHost: {host}\r\n\r\n'
+def get(host: str, path: str, header: str = '') -> str:
+    if header != '':
+        header = '\r\n' + header
+    message = f'GET {path} HTTP/1.0\r\nHost: {host}{header}\r\n\r\n'
     return request.req(host, 80, message)
--- Labs/Lab1/Code/Lab1/Lab1/httpc.py
+++ Labs/Lab1/Code/Lab1/Lab1/httpc.py
@@ -4,11 +4,12 @@ import post
 import urlparser
 parser = argparse.ArgumentParser(add_help=False)
 parser.add_argument("action", help="", choices=['get', 'post'])
-parser.add_argument("-v", help="", action='store_true')
-parser.add_argument("-h", help="", action='append')
-parser.add_argument("-d", help="")
+parser.add_argument("-help", help="", action='store_true')
+parser.add_argument("-v", help="Prints all the status, and its headers, then the contents of the response", action='store_true')
+parser.add_argument("-h", help="Passes the headers value to your HTTP operation", action='append')
+parser.add_argument("-d", help="Gives the user the possibility to associate the body of the HTTP Request with the inline data,")
 parser.add_argument("-f", help="")
-parser.add_argument("URL", help="")
+parser.add_argument("URL", help="Determines the targeted HTTP server. It could contain parameters of the HTTP operation. ")
 args = parser.parse_args()
 
 print(args)
@@ -23,7 +24,7 @@ header = '\r\n'.join(map(str,args.h))
 response = ''
 
 if action == 'get':
-    response = get.get(host, urlparser.get_path(url))
+    response = get.get(host, urlparser.get_path(url), header)
 elif action == 'post':
     response = post.post(host, urlparser.get_path(url), data, header)
 
