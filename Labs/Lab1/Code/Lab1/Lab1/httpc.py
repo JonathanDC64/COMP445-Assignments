661568d99872cb6c5e38c089c3c069322e32f3ec
JonathanDC64 <nintendo_jon64@hotmail.com>
2018-01-09 21:32:51 -0500

    progress made

--- Labs/Lab1/Code/Lab1/Lab1/httpc.py
+++ Labs/Lab1/Code/Lab1/Lab1/httpc.py
@@ -2,27 +2,30 @@ import argparse
 import get
 import post
 import urlparser
-
 parser = argparse.ArgumentParser(add_help=False)
 parser.add_argument("action", help="", choices=['get', 'post'])
 parser.add_argument("-v", help="", action='store_true')
-parser.add_argument("-h", help="")
+parser.add_argument("-h", help="", action='append')
 parser.add_argument("-d", help="")
 parser.add_argument("-f", help="")
 parser.add_argument("URL", help="")
 args = parser.parse_args()
 
+print(args)
+
+
+
 url = args.URL
 action = args.action
-
+data = args.d
 host = urlparser.get_host(url)
-
+header = '\r\n'.join(map(str,args.h))
 response = ''
 
 if action == 'get':
     response = get.get(host, urlparser.get_path(url))
 elif action == 'post':
-    response = post.post(host, urlparser.get_path(url), {})
+    response = post.post(host, urlparser.get_path(url), data, header)
 
 if args.v:
     print(response.get_header())
--- Labs/Lab1/Code/Lab1/Lab1/post.py
+++ Labs/Lab1/Code/Lab1/Lab1/post.py
@@ -1,10 +1,8 @@
 import request
 from typing import Dict
 
-def post(host: str, path: str, params: Dict) -> str:
-    body = ''
-    for key in params:
-        body += key + '=' + params[key] + '&'
-    body = body[:-1]
-    message = f'POST {path} HTTP/1.0\r\nHost: {host}\r\n\r\n{body}'
+def post(host: str, path: str, body: str, header: str = '') -> str:
+    if header != '':
+        header = '\r\n' + header
+    message = f'POST {path} HTTP/1.0\r\nHost: {host}\r\nContent-Length: {len(body)}{header}\r\n\r\n{body}'
     return request.req(host, 80, message)
