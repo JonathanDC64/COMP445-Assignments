74f730239a5798c2ae62d0fec36d85b1477a124a
JonathanDC64 <nintendo_jon64@hotmail.com>
2018-01-11 06:39:09 -0500

    tried some stuff

--- Labs/Lab1/Code/Lab1/Lab1/httpc.py
+++ Labs/Lab1/Code/Lab1/Lab1/httpc.py
@@ -2,16 +2,15 @@ import argparse
 import get
 import post
 import urlparser
-parser = argparse.ArgumentParser(add_help=False)
-parser.add_argument("action", help="", choices=['get', 'post'])
-parser.add_argument("-help", help="", action='store_true')
+parser = argparse.ArgumentParser(add_help=False, usage='httpc.py (get|post) [-v] (-h "k:v")* [-d inline-data] [-f file] URL')
+parser.add_argument("action", help="Executes a HTTP GET or POST request and prints the response.", choices=['get', 'post'])
+parser.add_argument("-help", help="Prints this screen.", action="help")
 parser.add_argument("-v", help="Prints all the status, and its headers, then the contents of the response", action='store_true')
 parser.add_argument("-h", help="Passes the headers value to your HTTP operation", action='append')
 parser.add_argument("-d", help="Gives the user the possibility to associate the body of the HTTP Request with the inline data,")
-parser.add_argument("-f", help="")
+parser.add_argument("-f", help="Send body data from file.")
 parser.add_argument("URL", help="Determines the targeted HTTP server. It could contain parameters of the HTTP operation. ")
 args = parser.parse_args()
-
 print(args)
 
 
