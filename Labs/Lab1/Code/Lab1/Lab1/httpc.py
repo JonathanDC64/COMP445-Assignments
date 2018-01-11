2cc6ff3ae650ce9b57a5d0fd3e1bb037b795112c
Jonathan Del Corpo <j_delc@encs.concordia.ca>
2018-01-11 11:02:30 -0500

    Fixed some mistakes and added test

--- Labs/Lab1/Code/Lab1/Lab1/httpc.py
+++ Labs/Lab1/Code/Lab1/Lab1/httpc.py
@@ -2,7 +2,9 @@ import argparse
 import get
 import post
 import urlparser
+
 parser = argparse.ArgumentParser(add_help=False, usage='httpc.py (get|post) [-v] (-h "k:v")* [-d inline-data] [-f file] URL')
+
 parser.add_argument("action", help="Executes a HTTP GET or POST request and prints the response.", choices=['get', 'post'])
 parser.add_argument("-help", help="Prints this screen.", action="help")
 parser.add_argument("-v", help="Prints all the status, and its headers, then the contents of the response", action='store_true')
@@ -10,16 +12,25 @@ parser.add_argument("-h", help="Passes the headers value to your HTTP operation"
 parser.add_argument("-d", help="Gives the user the possibility to associate the body of the HTTP Request with the inline data,")
 parser.add_argument("-f", help="Send body data from file.")
 parser.add_argument("URL", help="Determines the targeted HTTP server. It could contain parameters of the HTTP operation. ")
-args = parser.parse_args()
+
+args = parser.parse_args(['get', '-v', 'httpbin.org/ip'])
+
 print(args)
 
-
-
 url = args.URL
+
 action = args.action
-data = args.d
+
+data = ''
+if args.d:
+	data = args.d
+
 host = urlparser.get_host(url)
-header = '\r\n'.join(map(str,args.h))
+
+header = ''
+if args.h:
+	header = '\r\n'.join(map(str,args.h))
+	
 response = ''
 
 if action == 'get':
