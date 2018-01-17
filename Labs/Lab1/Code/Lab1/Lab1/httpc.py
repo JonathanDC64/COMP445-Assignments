d39d17e5c117a65fa9a89f75e5dc0550081334f9
JonathanDC64 <nintendo_jon64@hotmail.com>
2018-01-17 08:46:21 -0500

    seems to be working

--- Labs/Lab1/Code/Lab1/Lab1/httpc.py
+++ Labs/Lab1/Code/Lab1/Lab1/httpc.py
@@ -13,7 +13,7 @@ The commands are:
     help prints this screen."""
     
 GET_HELP = """
-usage: httpc get [-v] [-h key:value] URL
+Usage: httpc get [-v] [-h key:value] URL
 Get executes a HTTP GET request for a given URL.
     -v Prints the detail of the response such as protocol,
 status, and headers.
@@ -21,7 +21,7 @@ status, and headers.
 """
 
 POST_HELP = """
-usage: httpc post [-v] [-h key:value] [-d inline-data] [-f file] URL
+Usage: httpc post [-v] [-h key:value] [-d inline-data] [-f file] URL
 Post executes a HTTP POST request for a given URL with inline data or
 from file.
     -v Prints the detail of the response such as protocol, status, and headers.
@@ -32,10 +32,11 @@ from file.
 Either [-d] or [-f] can be used but not both.
 """
 
-
 parser = argparse.ArgumentParser(add_help=False, usage='httpc.py (get|post) [-v] (-h "k:v")* [-d inline-data] [-f file] URL')
 
-parser.add_argument("action", help="Executes a HTTP GET or POST request and prints the response.", choices=['get', 'post', 'none'], default='none', nargs='?')
+#parser.add_argument("action", help="Executes a HTTP GET or POST request and prints the response.", choices=['get', 'post', 'none'], default='none')
+parser.add_argument("-get", help="Executes a HTTP GET  request and prints the response.", action="store_true")
+parser.add_argument("-post", help="Executes a HTTP POST request and prints the response.", action="store_true")
 parser.add_argument("-help", help="Prints this screen.", action="store_true")
 parser.add_argument("-v", help="Prints all the status, and its headers, then the contents of the response", action='store_true')
 parser.add_argument("-h", help="Passes the headers value to your HTTP operation", action='append')
@@ -46,20 +47,22 @@ parser.add_argument("URL", help="Determines the targeted HTTP server. It could c
 #args = parser.parse_args(['post', '-v', 'httpbin.org/post', '-d', 'a=8'])
 #args = parser.parse_args(['get','-help'])
 args = parser.parse_args()
-print(args)
+#print(args)
 
 url = args.URL
 
-action = args.action
+#action = args.action
 
 data = ''
 if args.d:
     data = args.d
 
 parsedurl = urlparse(url)
+scheme = parsedurl.scheme
 host = parsedurl.netloc
 path = parsedurl.path
 query = parsedurl.query
+
 
 if query:
     path = path + '?' + query
@@ -72,22 +75,28 @@ help = args.help
 
 response = ''
 
-if action == 'get':
-    if help:
-        print(GET_HELP)
-    else:
-        response = get.get(host, path, header)
-elif action == 'post':
-    if help:
-        print(POST_HELP)
-    else:
-        response = post.post(host, path, data, header)
-elif help:
-    print(HELP)
+#print(args)
+#print(parsedurl)
 
-
-if not help:
-    if args.v:
-        print(response.get_header())
-    print(response.get_body())
+if help == False and (scheme != 'http' and scheme != 'https'):
+    parser.print_usage()
+    print("URL must contain either http or https protocol")
+else:
+    if args.get:
+        if help:
+            print(GET_HELP)
+        else:
+            response = get.get(host, path, header)
+    elif args.post:
+        if help:
+            print(POST_HELP)
+        else:
+            response = post.post(host, path, data, header)
+    elif help:
+        print(HELP)
+    
+    if not help:
+        if args.v:
+            print(response.get_header())
+        print(response.get_body())
     
