75064f4484ac479fb73bcdf528bc22d20c91593f
Jonathan Del Corpo <j_delc@encs.concordia.ca>
2018-01-17 12:52:58 -0500

    changed str to message

--- Labs/Lab1/Code/Lab1/Lab1/get.py
+++ Labs/Lab1/Code/Lab1/Lab1/get.py
@@ -1,9 +1,10 @@
 import request
+import message
 
 #Get request
-def get(host: str, path: str, header: str = '') -> str:
+def get(host: str, path: str, header: str = '') -> message:
     #Append header to message if any
     if header != '':
         header = '\r\n' + header
-    message = f'GET {path} HTTP/1.0\r\nHost: {host}{header}\r\n\r\n'
-    return request.req(host, 80, message)
+    msg = f'GET {path} HTTP/1.0\r\nHost: {host}{header}\r\n\r\n'
+    return request.req(host, 80, msg)
--- Labs/Lab1/Code/Lab1/Lab1/post.py
+++ Labs/Lab1/Code/Lab1/Lab1/post.py
@@ -1,9 +1,10 @@
 import request
+import message
 
 #POST request
-def post(host: str, path: str, body: str, header: str = '') -> str:
+def post(host: str, path: str, body: str, header: str = '') -> message:
     #Append header to message if any
     if header != '':
         header = '\r\n' + header
-    message = f'POST {path} HTTP/1.0\r\nHost: {host}\r\nContent-Length: {len(body)}{header}\r\n\r\n{body}'
-    return request.req(host, 80, message)
+    msg = f'POST {path} HTTP/1.0\r\nHost: {host}\r\nContent-Length: {len(body)}{header}\r\n\r\n{body}'
+    return request.req(host, 80, msg)
