0e9973be49f8542a71dc0f823ef53b9b01def1f7
JonathanDC64 <nintendo_jon64@hotmail.com>
2018-03-14 18:15:52 -0400

    done

--- Labs/Lab2/A/httpfs.py
+++ Labs/Lab2/A/httpfs.py
@@ -1,25 +1,90 @@
 import argparse
-import http_server
+import socket
+import threading
+import os
+import json
 
-parser = argparse.ArgumentParser(add_help=False, usage=' httpfs.py [-v] [-p PORT] [-d PATH-TO-DIR]')
-parser.add_argument("-v", help="Prints debugging messages.", action='store_true')
-parser.add_argument("-p", help="Specifies the port number that the server will listen and serve at.")
-parser.add_argument("-d", help="Specifies the directory that the server will use to read/write requested files. Default is the current directory when launching the application.")
+def run_server(host, port, v, d):
+	if not os.path.exists(d):
+		os.makedirs(d)
+	listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	try:
+		listener.bind((host, port))
+		listener.listen()
+		print('server is listening at ', p)
+		while True:
+			conn, addr = listener.accept()
+			threading.Thread(target=handle_client, args=(conn, addr, v, d)).start()
+	finally:
+		listener.close()
 
-#Get and validate the arguments
+mutex = threading.Lock()		
+buf = 2048		
+def handle_client(conn, addr, v, d):
+	if v:
+		print(f'Incomming client from ' + addr)
+	try:
+		while True:
+			data = conn.recv(buf)
+			if not data:
+				break
+				
+			delimiter = data.find(b'\r\n\r\n')
+			header = data[:delimiter].decode('utf-8')
+			body = data[delimiter+4:].decode('utf-8')
+			
+			lines = header.splitlines()
+			line1 = lines[0]
+			method = line1[:line1.find(' ')]
+			path = (line1[line1.find(' ') + 1 : line1.find(' ') + 1 + line1[line1.find(' ') + 1:].find(' ')])
+			relative_path = d + path
+			path = path + '/' if path != '/' else path
+			response_body = {}
+			code = '200'
+			if method == 'POST':
+				with mutex:
+					output = open(relative_path,"w")
+					output.write(body)
+					output.close()
+			elif method == 'GET':
+				if os.path.isfile(relative_path):
+					with mutex:
+						response_body['file'] = {'path': relative_path, 'contents': open(relative_path, 'r').read()}
+				elif os.path.isdir(relative_path):
+					files = os.listdir(relative_path)
+					response_body['files'] = files
+				else:
+					code = '404'
+			
+			
+			msg = ''
+			if code == '200':
+				msg = 'OK'
+			elif code == '404':
+				msg = 'Not Found'
+			
+			response_body['status'] = {'code': code, 'message': msg}
+			response_body = json.dumps(response_body, indent=4)
+			
+			response =  'HTTP/1.0 ' + code + ' ' + msg + '\r\n' + \
+			'Content-Length: ' + str(len(response_body)) + '\r\n' + \
+			'Content-Type: application/json\r\n' + \
+			'Content-Disposition: inline\r\n\r\n' + response_body
+
+			response = response.encode()
+			conn.sendall(response)
+				
+	finally:
+		if v:
+			print('Client closed connection from ' + addr)
+		conn.close()
+		
+parser = argparse.ArgumentParser()
+parser.add_argument("-d")
+parser.add_argument("-p")
+parser.add_argument("-v", action='store_true')
 args = parser.parse_args()
-
-verbose = args.v
-
-port = http_server.HTTP_PORT
-
-if args.p:
-    port = int(args.p)
-    
-directory = http_server.ROOT_DIRECTORY
-
-if args.d:
-    directory = args.d
-
-
-http_server.run_server(http_server.LOCALHOST, port, verbose, directory)
+v = args.v
+p = args.p if args.p else 80
+d = args.d if args.d else 'root'
+run_server('', p, v, d)	
